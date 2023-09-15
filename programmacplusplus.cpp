dc65af603904df59acef3ff73e8f36443898893a
Matt32008 <91756048+Matt3Matrix@users.noreply.github.com>
2023-09-15 21:05:15 +0200

    Add files via upload

---
+++ programmacplusplus.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+cout << "     .... NO! ...                  ... MNO! ..." << endl;
+cout << "   ..... MNO!! ...................... MNNOO! ..." << endl;
+cout << " ..... MMNO! ......................... MNNOO!! ." << endl;
+cout << ".... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! ." << endl;
+cout << " ... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ...." << endl;
+cout << "    ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ..." << endl;
+cout << "   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! ....." << endl;
+cout << "   ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...  " << endl;
+cout << "    ....... MMMMM..    OPPMMP    .,OMI! ...." << endl;
+cout << "     ...... MMMM::   o.,OPMP,.o   ::I!! ..." << endl;
+cout << "         .... NNM:::.,,OOPM!P,.::::!! ...." << endl;
+cout << "          .. MMNNNNNOOOOPMO!!IIPPO!!O! ....." << endl;
+cout << "         ... MMMMMNNNNOO:!!:!!IPPPPOO! ...." << endl;
+cout << "           .. MMMMMNNOOMMNNIIIPPPOO!! ......" << endl;
+cout << "          ...... MMMONNMMNNNIIIOO!.........." << endl;
+cout << "       ....... MN MOMMMNNNIIIIIO! OO .........." << endl;
+cout << "    ......... MNO! IiiiiiiiiiiiI OOOO ..........." << endl;
+cout << "  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........" << endl;
+cout << "   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........" << endl;
+cout << "   ...... MNNNNO! .. PPPPPPPPP .. MMNON!........" << endl;
+cout << "      ...... OO! ................. ON! ......." << endl;
+cout << "         ................................" << endl << endl;
+cout << "             WELCOME TO THE DEDSEC OS                    " << endl << endl;
+    return 0;
+}
